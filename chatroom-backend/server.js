0a9e7bfa7c602425237e508b33d573ff93766d53
Vmihira <vinjamurimihira@gmail.com>
2025-06-20 17:32:13 +0530

    updated few routes

--- chatroom-backend/server.js
+++ chatroom-backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'https://kuvaka-frontend-f8c7fwcw8-mihiras-projects.vercel.app',
+    origin: 'https://kuvaka-frontend-nine.vercel.app/',
     methods: ["GET", "POST"]
   }
 });
